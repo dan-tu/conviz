f65ca0d2bd59cd551e378ae4a3e011ea7e859560
Dan Tu <23713851+dan-tu@users.noreply.github.com>
2024-02-09 18:33:14 -0800

    Add image picker

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let file, canvas, viz;
+
+    // File -> DataURL -> Image -> Canvas
+    function onSelectImage(e) {
+        if (e.target.files.length) {
+            file = e.target.files[0];
+        }
+    }
+</script>
+
+<div>
+    <h1>2D Convolution Visualizer</h1>
+    <h2>Step 1: Upload an Image</h2>
+    <form>
+        <label for="sourceImg">Choose an image:</label>
+        <input type="file" id="sourceImg" accept=".png, .jpeg, .jpg" required on:change={onSelectImage} />
+    </form>
+
+    {#if file}
+        <h2>Step 2: Define a Filter</h2>
+
+    {/if}
+</div>
+
+<style>
+
+
+</style>
